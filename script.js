c9f3fe72acef4290212f97855a214c30043c3993
Zilcon <akubo161333@gmail.com>
2025-10-07 23:37:34 +0900

    Update script.js

--- script.js
+++ script.js
@@ -237,7 +237,7 @@ async function saveData() {
   const countryDataToSave = countryall;
   
   try {
-    const response = await fetch("GASのURL", { // ※必ずご自身のGASのURLに書き換えてください
+    const response = await fetch("https://script.google.com/macros/s/AKfycby5r0RKGNhU8MPwsr1NOz8qhuLzmM0u7JXp2hr9AVBlKhRtttWR-7RilToi0_ZlznnAvw/exec", { // ※必ずご自身のGASのURLに書き換えてください
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ MapData: mapDataToSave, CountryData: countryDataToSave }),
