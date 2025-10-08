232310dc087adc28e6ca0e3f352fd497158aeb31
Zilcon <akubo161333@gmail.com>
2025-10-08 17:38:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let newCountryInfo = { name: '', color: '' };
 let selectedPathsForNewCountry = [];
 
 // ===== データ取得と初期設定 =====
-fetch("https://script.google.com/macros/s/AKfycbxHPT0-MWsTZGgp1Eq7-RSmBJTz0lHuZbI3iIKewrdnSs-NQtoM4ZNuBgeKvoasoRlE/exec") // ※ご自身のGASのURLに書き換えてください
+fetch("https://script.google.com/macros/s/AKfycbzcbYnpVjJ8WUn6F6asZFvh5EqGGdPism52vtbEd0LyNGtk-Clcnd4dIGi3W8i-dINY2w/exec") // ※ご自身のGASのURLに書き換えてください
   .then(res => res.ok ? res.json() : Promise.reject(new Error('Network response was not ok.')))
   .then(data => {
     const mapContainer = document.getElementById('map-container');
@@ -252,7 +252,7 @@ async function saveData() {
     const mapDataToSave = [pagename, ...maprows];
     const countryDataToSave = countryall;
     try {
-        const response = await fetch("https://script.google.com/macros/s/AKfycbxHPT0-MWsTZGgp1Eq7-RSmBJTz0lHuZbI3iIKewrdnSs-NQtoM4ZNuBgeKvoasoRlE/exec", {
+        const response = await fetch("https://script.google.com/macros/s/AKfycbzcbYnpVjJ8WUn6F6asZFvh5EqGGdPism52vtbEd0LyNGtk-Clcnd4dIGi3W8i-dINY2w/exec", {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ MapData: mapDataToSave, CountryData: countryDataToSave }),
